74cc4b692f26ab856788bd5a16f30cdfda19802e
andrey <andreases_mobimoz@ukr.net>
2022-12-07 18:57:56 +0200

    added dushboard page, filter-change-view btn, and teble view

--- src/screens/dashboard/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from '../../shared/Layout';
-
-export default function DashboardPage() {
-  return (
-    <Layout title="Dashboard">
-      <div>DashboardPage</div>
-    </Layout>
-  );
-}
